39d3e0d085e2d476fca7e91ed8693c139e3bba88
RZIW <mrs.safari20@gmail.com>
2020-03-17 13:42:16 +0330

    add nessessary fields for user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('family')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile_number')->nullable();
+            $table->string('home_number')->nullable();
+            $table->integer('postal_code')->nullable();
+            $table->text('address')->nullable();
+            $table->string('role')->default('customer');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
