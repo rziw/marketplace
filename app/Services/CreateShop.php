dbe1a96556c2491881f50a0b88a5e1ca75cab508
RZIW <mrs.safari20@gmail.com>
2020-03-18 00:25:55 +0330

    notify user that his/her market has been added , but mock sending email

--- app/Services/CreateShop.php
+++ app/Services/CreateShop.php
@@ -5,11 +5,16 @@ namespace App\Services;
 
 
 use App\Models\Shop;
+use App\Models\User;
+use App\Notifications\MarketAdded;
+use Illuminate\Support\Facades\Mail;
 
 class CreateShop
 {
-    public function store($user)
+    public function store(User $user)
     {
         Shop::updateOrCreate(['owner_id' => $user->id]);
+        Mail::fake();//Mock sending email unless all requirements of sending email are OK
+        $user->notify(new MarketAdded());
     }
 }
