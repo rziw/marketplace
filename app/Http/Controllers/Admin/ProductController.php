2113eb905b1dfe49263d5d87d2a7e41eebd07a58
RZIW <mrs.safari20@gmail.com>
2020-03-21 05:13:28 +0430

    impelement CRUD of product via admin

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\UpdateProductRequest;
+use App\Models\Product;
+use App\Models\Shop;
+use App\Notifications\ProductChanged;
+use Illuminate\Support\Facades\Mail;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Shop $shop
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Shop $shop)
+    {
+        $products = $shop->products;
+        return response()->json(compact('products'));
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Shop $shop
+     * @param  Product  $product
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($shop, Product $product)
+    {
+        return response()->json(compact('product'));
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  UpdateProductRequest  $request
+     * @param  Product $product
+     * @param  Shop $shop
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(UpdateProductRequest $request,Shop $shop, Product $product)
+    {
+        $product_input = $request->only(['name', 'description', 'tag']);
+        $product_shop_input = $request->except(['name', 'description', 'tag']);
+
+        $product->update($product_input);
+        $shop->products()->updateExistingPivot($product, $product_shop_input);
+        $this->checkForNotifyUser($shop, $product, $request);
+
+        return response()->json(['message'=> 'You have successfully updated the product.']);
+    }
+
+    private function checkForNotifyUser(Shop $shop, Product $product, UpdateProductRequest $request)
+    {
+        if(isset($request->status) && $product->status != $request->status) {
+            Mail::fake();//Mock sending email unless all requirements of sending email are OK
+            $message = "status of shop updated to $request->status.";
+            $shop->user->notify(new ProductChanged($message));
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Shop $shop
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function destroy($shop, Product $product)
+    {
+        $product->delete();
+        $shop->products()->detach($product);
+
+        return response()->json(['message'=> 'You have successfully deleted the product.']);
+    }
+}
---
+++ app/Http/Requests/Admin/UpdateProductRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use App\Traits\Validation\HandleFailedValidation;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    use HandleFailedValidation;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:3|max:100',
+            'description' => 'required|min:10|max:2000',
+            'color' => 'required|min:2|max:255',
+            'count' => 'required|integer|min:0',
+            'price' => 'required|integer|min:100',
+            'has_guarantee' => 'sometimes|required|boolean',
+            'guarantee_description' => 'required_if:has_guarantee,1',
+            'status' => 'in:accepted,waiting',
+        ];
+    }
+}
