b3485edaa80d66e46056eadfbe1484303e1abbf0
RZIW <mrs.safari20@gmail.com>
2020-03-21 15:45:10 +0430

    use renamed notification class

--- app/Services/CreateShop.php
+++ app/Services/CreateShop.php
@@ -6,7 +6,7 @@ namespace App\Services;
 
 use App\Models\Shop;
 use App\Models\User;
-use App\Notifications\MarketChange;
+use App\Notifications\MarketChanged;
 use Illuminate\Support\Facades\Mail;
 
 class CreateShop
@@ -16,6 +16,6 @@ class CreateShop
         Shop::updateOrCreate(['owner_id' => $user->id]);
         Mail::fake();//Mock sending email unless all requirements of sending email are OK
         $message = "You are a seller now and you have your own market in our system!";
-        $user->notify(new MarketChange($message));
+        $user->notify(new MarketChanged($message));
     }
 }
