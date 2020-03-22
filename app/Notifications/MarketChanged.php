d828843bece8b39a4c4ffc33f060325267d520ca
RZIW <mrs.safari20@gmail.com>
2020-03-22 21:28:16 +0430

    modify class name

--- app/Notifications/MarketChanged.php
+++ app/Notifications/MarketChanged.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Mail;
 
-class MarketChange extends Notification
+class MarketChanged extends Notification
 {
     use Queueable;
     private $message;
