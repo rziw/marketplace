832e131120169bbfc923172f28bfc102b99ea356
RZIW <mrs.safari20@gmail.com>
2020-03-22 14:55:31 +0430

    use new name of repository instead of previous

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,57 +13,57 @@ class HomeController extends Controller
      * Display a listing of the resource.
      *
      * @param  Request $request
-     * @param ProductRepository $shopRepository
+     * @param ProductRepository $productRepository
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index(Request $request, ProductRepository $shopRepository)
+    public function index(Request $request, ProductRepository $productRepository)
     {
         try {
             $user = JWTAuth::parseToken()->authenticate();
 
             if ($user) {
-                $products = $this->listForAuthenticatedUser($request, $shopRepository, $user);
+                $products = $this->listForAuthenticatedUser($request, $productRepository, $user);
             } else {//token is invalid for any reason
-                $products = $this->listForNotAuthenticatedUser($request, $shopRepository);
+                $products = $this->listForNotAuthenticatedUser($request, $productRepository);
             }
 
         } catch (JWTException $e) {//token not set
-            $products = $this->listForNotAuthenticatedUser($request, $shopRepository);
+            $products = $this->listForNotAuthenticatedUser($request, $productRepository);
         }
 
         return response()->json(compact('products'));
     }
 
-    public function listForNotAuthenticatedUser($request, $shopRepository)
+    public function listForNotAuthenticatedUser($request, $productRepository)
     {
         if ($request->has('lat') && !is_null($request->lat)
             && $request->has('lng') && !is_null($request->lng)
             && $request->has('radius') && !is_null($request->radius)) {
 
-            $products = $shopRepository->listClosest($request->lat, $request->lng, $request->radius);
+            $products = $productRepository->listClosest($request->lat, $request->lng, $request->radius);
 
         } else {
-            $products = $shopRepository->list();
+            $products = $productRepository->list();
         }
 
         return $products;
     }
 
-    public function listForAuthenticatedUser($request, $shopRepository, $user)
+    public function listForAuthenticatedUser($request, $productRepository, $user)
     {
         if (!is_null($user->latitude) && !is_null($user->longitude)) {
 
-            $products = $shopRepository->listClosest($user->latitude, $user->longitude, $user->radius);
+            $products = $productRepository->listClosest($user->latitude, $user->longitude, $user->radius);
 
         } elseif ($request->has('lat') && !is_null($request->lat)
             && $request->has('lng') && !is_null($request->lng)
             && $request->has('radius') && !is_null($request->radius)
         ) {
 
-            $products = $shopRepository->listClosest($request->lat, $request->lng, $request->radius);
+            $products = $productRepository->listClosest($request->lat, $request->lng, $request->radius);
 
         } else {
-            $products = $shopRepository->list();
+            $products = $productRepository->list();
         }
 
         return $products;
