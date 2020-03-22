893dbed1bc03a9784738a41d7a33efe64bdda353
RZIW <mrs.safari20@gmail.com>
2020-03-22 15:57:11 +0430

    refactor controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,61 +9,68 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 
 class HomeController extends Controller
 {
+    private $request;
+    private $productRepository;
+
+    public function __construct(Request $request, ProductRepository $productRepository)
+    {
+        $this->request = $request;
+        $this->productRepository = $productRepository;
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @param  Request $request
-     * @param ProductRepository $productRepository
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index(Request $request, ProductRepository $productRepository)
+    public function index()
     {
         try {
             $user = JWTAuth::parseToken()->authenticate();
 
             if ($user) {
-                $products = $this->listForAuthenticatedUser($request, $productRepository, $user);
+                $products = $this->listForAuthenticatedUser($user);
             } else {//token is invalid for any reason
-                $products = $this->listForNotAuthenticatedUser($request, $productRepository);
+                $products = $this->listForNotAuthenticatedUser();
             }
 
         } catch (JWTException $e) {//token not set
-            $products = $this->listForNotAuthenticatedUser($request, $productRepository);
+            $products = $this->listForNotAuthenticatedUser();
         }
 
         return response()->json(compact('products'));
     }
 
-    public function listForNotAuthenticatedUser($request, $productRepository)
+    public function listForNotAuthenticatedUser()
     {
-        if ($request->has('lat') && !is_null($request->lat)
-            && $request->has('lng') && !is_null($request->lng)
-            && $request->has('radius') && !is_null($request->radius)) {
+        if ($this->request->has('lat') && !is_null($this->request->lat)
+            && $this->request->has('lng') && !is_null($this->request->lng)
+            && $this->request->has('radius') && !is_null($this->request->radius)) {
 
-            $products = $productRepository->listClosest($request->lat, $request->lng, $request->radius);
+            $products = $this->productRepository->listClosest($this->request->lat, $this->request->lng, $this->request->radius);
 
         } else {
-            $products = $productRepository->list();
+            $products = $this->productRepository->list();
         }
 
         return $products;
     }
 
-    public function listForAuthenticatedUser($request, $productRepository, $user)
+    public function listForAuthenticatedUser( $user)
     {
         if (!is_null($user->latitude) && !is_null($user->longitude)) {
 
-            $products = $productRepository->listClosest($user->latitude, $user->longitude, $user->radius);
+            $products = $this->productRepository->listClosest($user->latitude, $user->longitude, $user->radius);
 
-        } elseif ($request->has('lat') && !is_null($request->lat)
-            && $request->has('lng') && !is_null($request->lng)
-            && $request->has('radius') && !is_null($request->radius)
+        } elseif ($this->request->has('lat') && !is_null($this->request->lat)
+            && $this->request->has('lng') && !is_null($this->request->lng)
+            && $this->request->has('radius') && !is_null($this->request->radius)
         ) {
 
-            $products = $productRepository->listClosest($request->lat, $request->lng, $request->radius);
+            $products = $this->productRepository->listClosest($this->request->lat, $this->request->lng, $this->request->radius);
 
         } else {
-            $products = $productRepository->list();
+            $products = $this->productRepository->list();
         }
 
         return $products;
