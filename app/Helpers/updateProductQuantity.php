f60ea55685678b615d5fffa6f860520f9f1207ed
RZIW <mrs.safari20@gmail.com>
2020-03-23 20:14:21 +0430

    update quantity of products in a separate class cause it needs to happen in an irrelated class during updating order!

---
+++ app/Helpers/updateProductQuantity.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Helpers;
+
+
+use App\Events\ProductFinished;
+use App\Models\Shop;
+
+class updateProductQuantity
+{
+    public function updateQuantity($order)
+    {
+        foreach ($order->orderproducts as $orderproduct) {
+            $shop = Shop::where('id', $orderproduct->shop_id)->first();
+            $product = $shop->products->where('id', $orderproduct->product_id)->first();
+
+            $product->pivot->count = $product->pivot->count - $orderproduct->count;
+            $product->pivot->save();
+
+            if($product->pivot->count == 0) {
+                event(new ProductFinished($product));
+            }
+        }
+    }
+}
