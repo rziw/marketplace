446015d5bc0a9d2427d19bd0a94067cc0c32d1ca
rziw <mrs.safari20@gmail.com>
2021-03-30 15:20:55 +0430

    refactor : add const of statuses of shop in it's model

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -8,6 +8,8 @@ class Shop extends Model
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
+    const STATUSES = ['accepted' => 0];
+
     public function user()
     {
         return $this->belongsTo('App\Models\User', 'owner_id');
